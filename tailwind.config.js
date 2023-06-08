d4d9ca7e69a40b76d17ee35a7fda15e8352414cf
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-08 18:26:19 -0400

    added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'main': '#F60954',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
