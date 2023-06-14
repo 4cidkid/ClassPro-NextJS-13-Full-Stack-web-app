ecabf3ddfd7a7492638ddb57515378cf12fd1ad0
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-13 23:27:21 -0400

    playing with backend data

--- app/tutors/search.js
+++ app/tutors/search.js
@@ -1,16 +1,12 @@
 async function searchFor({ subject, level, min, max }) {
   let query = undefined;
   if (subject && level && min && max) {
-    query = {
-      subject,
-      level,
-      min,
-      max,
-    };
+    const data = await fetch(
+      `http://localhost:3000/api/tutors?subject=${subject}&level=${level}&min=${min}&max=${max}`
+      ,
+    );
   }
   return query;
 }
-
-
 
 export default searchFor;
