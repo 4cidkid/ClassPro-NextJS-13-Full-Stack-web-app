3d7f88832b9a6d1bb6d3c3bcca1c06ec3fde87e0
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-14 21:39:53 -0400

    changed directory to app folder

--- app/querys.js
+++
@@ -1,8 +0,0 @@
-export const selectAdvertisements = (subject,min,max,level) => {
-  return `
-  SELECT advertisements.advertisements_title, advertisements.advertisements_description, advertisements.advertisements_level, advertisements.advertisements_price, tutors.tutor_name, subjects.subject_name
-  FROM advertisements
-  JOIN tutors ON advertisements.tutor_id = tutors.tutor_id AND advertisements.advertisements_price BETWEEN ${min}::money AND ${max}::money
-  JOIN subjects ON advertisements.subject_id = subjects.subject_id AND subjects.subject_name = '${subject}' AND advertisements.advertisements_level = '${level.toLowerCase()}';
-`;
-};
