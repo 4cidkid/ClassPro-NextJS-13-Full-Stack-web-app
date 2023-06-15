98de5f7c68a9ac12b8ca4858d05ca68479b17dd0
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-15 19:25:21 -0400

    when page is loading, scroll is disabled

--- components/common/loading.jsx
+++ components/common/loading.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-
-export const Loader = () => {
-    return(
-        <div className='loader-container'>
-            <p className='text-4xl text-white font-bold'>Loading Your Awesome Tutors!</p>
-            <span className="loader"></span>
-        </div>
-    )
-}
+export const Loader = (props) => {
+  useEffect(() => {
+    if (!props.show) {
+      document.body.style.height = "100vh";
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.height = "fit-content";
+      document.body.style.overflowY = "";
+    }
+  }, [props.show]);
+  return (
+    <div className="loader-container z-[10]">
+      <p className="text-4xl text-white font-bold">
+        Loading Your Awesome Tutors!
+      </p>
+      <span className="loader"></span>
+    </div>
+  );
+};
