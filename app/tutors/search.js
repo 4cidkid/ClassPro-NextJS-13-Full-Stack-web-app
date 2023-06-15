69e806d7227af2283a90071806755ba3e3f8705b
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-15 00:20:30 -0400

    added two functions instead of one to avoid complexity

--- app/tutors/search.js
+++ app/tutors/search.js
@@ -1,17 +1,13 @@
-import TutorsCards from "@/components/tutors/tutorsCard";
-
-async function searchFor({ subject, level, min, max }) {
-  if (subject && level && min && max) {
-    const response = await fetch(
-      `http://localhost:3000/api/tutors?subject=${subject}&level=${level}&min=${min}&max=${max}`
-    );
-    const data = await response.json();
-    return data.response;
-  } else {
-    const response = await fetch(`http://localhost:3000/api/tutors`);
-    const data = await response.json();
-    return data.response;
-  }
+export async function searchFor({ subject, level, min, max }) {
+  const response = await fetch(
+    `http://localhost:3000/api/tutors?subject=${subject}&level=${level}&min=${min}&max=${max}`
+  );
+  const data = await response.json();
+  return data.response;
+}
+export async function searchAny() {
+  const response = await fetch(`http://localhost:3000/api/tutors`);
+  const data = await response.json();
+  return data.response;
 }
 
-export default searchFor;
