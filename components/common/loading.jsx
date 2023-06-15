2facea9675a5207d00cac3e7e7d10f514dfb4b9c
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-14 23:32:04 -0400

    added loading component

---
+++ components/common/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const Loader = () => {
+    return(
+        <div className='loader-container'>
+            <p className='text-4xl text-white font-bold'>Loading Your Awesome Tutors!</p>
+            <span className="loader"></span>
+        </div>
+    )
+}
