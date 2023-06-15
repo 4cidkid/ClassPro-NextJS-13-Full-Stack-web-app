14198f17b0cb87ce8ff2fdfd6fef6379de5214ef
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-15 01:55:46 -0400

    added country query for selectAdvertisements and fixed query too

--- app/api/querys.js
+++ app/api/querys.js
@@ -1,14 +1,16 @@
 export const selectAdvertisements = (subject, min, max, level) => {
   return `
-  SELECT advertisements.advertisements_title, advertisements.advertisements_description, advertisements.advertisements_level, advertisements.advertisements_price, tutors.tutor_name,tutors.tutor_lastname, subjects.subject_name
+  SELECT advertisements.advertisements_title,country.iso,tutors.tutor_description,tutors.tutor_skills, advertisements.advertisements_description, advertisements.advertisements_level, advertisements.advertisements_price, tutors.tutor_name,tutors.tutor_lastname, subjects.subject_name
   FROM advertisements
   JOIN tutors ON advertisements.tutor_id = tutors.tutor_id AND advertisements.advertisements_price BETWEEN ${min}::money AND ${max}::money
-  JOIN subjects ON advertisements.subject_id = subjects.subject_id AND subjects.subject_name = '${subject}' AND advertisements.advertisements_level = '${level.toLowerCase()}';
+  JOIN subjects ON advertisements.subject_id = subjects.subject_id AND subjects.subject_name = '${subject}' AND advertisements.advertisements_level = '${level.toLowerCase()}'
+  JOIN country ON tutors.country_id = country.id
+  ORDER BY advertisements.advertisements_price;
 `;
 };
 export const selectAnyAdvertisements = () => {
   return `
-  SELECT advertisements.advertisements_title,country.iso, advertisements.advertisements_description, advertisements.advertisements_level, advertisements.advertisements_price, tutors.tutor_name,tutors.tutor_lastname, subjects.subject_name
+  SELECT advertisements.advertisements_title,country.iso,tutors.tutor_description,tutors.tutor_skills, advertisements.advertisements_description, advertisements.advertisements_level, advertisements.advertisements_price, tutors.tutor_name,tutors.tutor_lastname, subjects.subject_name
   FROM advertisements
   JOIN tutors ON advertisements.tutor_id = tutors.tutor_id
   JOIN subjects ON advertisements.subject_id = subjects.subject_id
