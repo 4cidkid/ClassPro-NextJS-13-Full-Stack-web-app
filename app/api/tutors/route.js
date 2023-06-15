9d1aa8f5f2010e77d6414d61e60ea72d2449dffe
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-15 19:30:19 -0400

    modified nextresponse, for synthetizing response and added respective functions to each request

--- app/api/tutors/route.js
+++ app/api/tutors/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import pool from "../db";
-import { selectAdvertisements,selectAnyAdvertisements } from "../querys";
+import { selectAdvertisements,selectAnyAdvertisements,selectTutorLanguages } from "../querys";
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams;
 
@@ -9,10 +9,19 @@ export async function GET(request) {
   const min = searchParams.get("min");
   const max = searchParams.get("max");
   let response;
-  if(subject && level && min && max){
-    response =  await pool.query(selectAdvertisements(subject,min,max,level))
-  }else{
-    response = await pool.query(selectAnyAdvertisements())
+  let language;
+  try{
+    if(subject && level && min && max){
+      response =  await pool.query(selectAdvertisements(subject,min,max,level))
+      language = await pool.query(selectTutorLanguages())
+    }else{
+      response = await pool.query(selectAnyAdvertisements())
+      language = await pool.query(selectTutorLanguages(subject, min, max, level))
+    }
+  }catch(err) 
+  {
+    console.log(err)
   }
-  return NextResponse.json({ response: response.rows });
+
+  return NextResponse.json({ response: response.rows,language: language.rows });
 }
