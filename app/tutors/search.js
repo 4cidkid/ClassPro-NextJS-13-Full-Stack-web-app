50f39eb15879dc50bde5ed60bab87c8419e40143
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-14 21:40:45 -0400

    added fetching api endpoints for tutors data

--- app/tutors/search.js
+++ app/tutors/search.js
@@ -1,12 +1,18 @@
+import TutorsCards from "@/components/tutors/tutorsCard";
+
 async function searchFor({ subject, level, min, max }) {
   let query = undefined;
   if (subject && level && min && max) {
-    const data = await fetch(
+    const response = await fetch(
       `http://localhost:3000/api/tutors?subject=${subject}&level=${level}&min=${min}&max=${max}`
-      ,
     );
+    const data = await response.json();
+    return data.response;
+  } else {
+    const response = await fetch(`http://localhost:3000/api/tutors`);
+    const data = await response.json();
+    return data.response;
   }
-  return query;
 }
 
 export default searchFor;
