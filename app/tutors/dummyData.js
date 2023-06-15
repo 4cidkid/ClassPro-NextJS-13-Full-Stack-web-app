b1507a5138ab7dda257c660aec521083ddf75016
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-15 19:22:24 -0400

    added dummy data to display something on screen while tutors load

---
+++ app/tutors/dummyData.js
@@ -0,0 +1,102 @@
+export const dummyData = [
+  {
+    id: 1,
+    ad_desc: "learn what you want...",
+    ad_level: "beginner",
+    ad_price: "$28",
+    ad_title: "Ace Your Chemistry Exams",
+    country: "OM",
+    subject: "Chemistry",
+    tu_desc:
+      "Specialized in providing guidance for exam preparation and test-taking strategies.",
+    tu_lastname: "Smith",
+    tu_name: "Henry",
+    tu_skill: (5)[
+      ("Leadership",
+      "Project management",
+      "Languages",
+      "Problem-solving",
+      "Digital marketing")
+    ],
+  },
+  {
+    id: 2,
+    ad_desc: "learn what you want...",
+    ad_level: "beginner",
+    ad_price: "$28",
+    ad_title: "Ace Your Chemistry Exams",
+    country: "OM",
+    subject: "Chemistry",
+    tu_desc:
+      "Specialized in providing guidance for exam preparation and test-taking strategies.",
+    tu_lastname: "Smith",
+    tu_name: "Henry",
+    tu_skill: (5)[
+      ("Leadership",
+      "Project management",
+      "Languages",
+      "Problem-solving",
+      "Digital marketing")
+    ],
+  },
+  {
+    id: 3,
+    ad_desc: "learn what you want...",
+    ad_level: "beginner",
+    ad_price: "$28",
+    ad_title: "Ace Your Chemistry Exams",
+    country: "OM",
+    subject: "Chemistry",
+    tu_desc:
+      "Specialized in providing guidance for exam preparation and test-taking strategies.",
+    tu_lastname: "Smith",
+    tu_name: "Henry",
+    tu_skill: (5)[
+      ("Leadership",
+      "Project management",
+      "Languages",
+      "Problem-solving",
+      "Digital marketing")
+    ],
+  },
+  {
+    id: 4,
+    ad_desc: "learn what you want...",
+    ad_level: "beginner",
+    ad_price: "$28",
+    ad_title: "Ace Your Chemistry Exams",
+    country: "OM",
+    subject: "Chemistry",
+    tu_desc:
+      "Specialized in providing guidance for exam preparation and test-taking strategies.",
+    tu_lastname: "Smith",
+    tu_name: "Henry",
+    tu_skill: (5)[
+      ("Leadership",
+      "Project management",
+      "Languages",
+      "Problem-solving",
+      "Digital marketing")
+    ],
+  },
+  {
+    id: 5,
+    ad_desc: "learn what you want...",
+    ad_level: "beginner",
+    ad_price: "$28",
+    ad_title: "Ace Your Chemistry Exams",
+    country: "OM",
+    subject: "Chemistry",
+    tu_desc:
+      "Specialized in providing guidance for exam preparation and test-taking strategies.",
+    tu_lastname: "Smith",
+    tu_name: "Henry",
+    tu_skill: (5)[
+      ("Leadership",
+      "Project management",
+      "Languages",
+      "Problem-solving",
+      "Digital marketing")
+    ],
+  },
+];
