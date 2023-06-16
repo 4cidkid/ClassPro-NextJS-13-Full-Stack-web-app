a77ffe173fa231c0fb75bedfd10018f966b2a994
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-15 23:45:16 -0400

    added logic for the filters

--- app/api/tutors/route.js
+++ app/api/tutors/route.js
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
 import pool from "../db";
-import { selectAdvertisements,selectAnyAdvertisements,selectTutorLanguages,selectTutorLanguagesFilter } from "../querys";
+import {
+  selectAdvertisements,
+  selectAnyAdvertisements,
+  selectTutorLanguages,
+  selectTutorLanguagesFilter,
+  selectAdvertisementsAll,
+  selectTutorLanguagesAll,
+  selectTutorLanguageLevel,
+  selectAdvertisementsLevel,
+  selectTutorLanguageMin,
+  selectAdvertisementsMin,
+} from "../querys";
 export async function GET(request) {
   const searchParams = request.nextUrl.searchParams;
 
@@ -10,19 +21,38 @@ export async function GET(request) {
   const max = searchParams.get("max");
   let response;
   let language;
-  try{
-    if(subject && level && min && max){
-      response =  await pool.query(selectAdvertisements(subject,min,max,level))
-      language = await pool.query(selectTutorLanguagesFilter(subject, min, max, level))
-      
-    }else{
-      response = await pool.query(selectAnyAdvertisements())
-      language = await pool.query(selectTutorLanguages())
+
+  if (subject && level && min && max) {
+    if (subject.toLowerCase() === "all" && level.toLowerCase() != "all") {
+      response = await pool.query(selectAdvertisementsAll(min, max, level));
+      language = await pool.query(selectTutorLanguagesAll(min, max, level));
+    } else if (
+      level.toLowerCase() === "all" &&
+      subject.toLowerCase() != "all"
+    ) {
+      response = await pool.query(selectAdvertisementsLevel(subject, min, max));
+      language = await pool.query(selectTutorLanguageLevel(subject, min, max));
+    } else if (
+      level.toLowerCase() === "all" &&
+      subject.toLowerCase() === "all"
+    ) {
+      response = await pool.query(selectAdvertisementsMin(min, max));
+      language = await pool.query(selectTutorLanguageMin(min, max));
+    } else {
+      response = await pool.query(
+        selectAdvertisements(subject, min, max, level)
+      );
+      language = await pool.query(
+        selectTutorLanguagesFilter(subject, min, max, level)
+      );
     }
-  }catch(err) 
-  {
-    console.log(err)
+  } else {
+    response = await pool.query(selectAnyAdvertisements());
+    language = await pool.query(selectTutorLanguages());
   }
 
-  return NextResponse.json({ response: response.rows,language: language.rows });
+  return NextResponse.json({
+    response: response.rows,
+    language: language.rows,
+  });
 }
