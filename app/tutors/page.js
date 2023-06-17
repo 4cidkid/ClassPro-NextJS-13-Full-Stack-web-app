835e307be2f906b0ec66904b23253f6b1e5d9e2e
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-16 21:50:01 -0400

    added pagination to tutors

--- app/tutors/page.js
+++ app/tutors/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { Search } from "react-feather";
+import { ChevronLeft, ChevronRight, Search } from "react-feather";
 import { useSearchParams } from "next/navigation";
 import Router from "next/router";
 import { searchFor, searchAny } from "./search";
@@ -54,6 +54,17 @@ export function Tutors() {
 const GridTutors = ({ subject, level, min, max }) => {
   const [dataApi, setData] = useState();
   const [falseData, setFalseData] = useState(false);
+  const [defaultPage, setDefaultPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [slice, setSlice] = useState({ start: 0, end: 6 });
+  console.log(slice);
+  let mapMe = [];
+  if (totalPages != 0) {
+    for (let i = 0; i <= totalPages - 1; i++) {
+      mapMe.push(i);
+    }
+  }
+  console.log(mapMe);
   useEffect(() => {
     async function fetchData() {
       if (subject && level && min && max) {
@@ -66,9 +77,11 @@ const GridTutors = ({ subject, level, min, max }) => {
         if (data === false) {
           const data = await searchAny();
           setData(data);
+          setTotalPages(Math.round(data.response.length / 6));
           setFalseData(true);
         } else {
           setFalseData(false);
+          setTotalPages(Math.round(data.response.length / 6));
           setData(data);
         }
       }
@@ -80,6 +93,7 @@ const GridTutors = ({ subject, level, min, max }) => {
       if (!subject || !level || !min || !max) {
         const data = await searchAny();
         setFalseData(false);
+        setTotalPages(Math.round(data.response.length / 6));
         setData(data);
       }
     }
@@ -97,17 +111,17 @@ const GridTutors = ({ subject, level, min, max }) => {
             <p>We didn't find any tutor, but Here is some Nice Tutors!</p>
           )}
           {dataApi &&
-            dataApi.response?.map((tutor) => {
+            dataApi.response?.slice(slice.start, slice.end).map((tutor, i) => {
               const languages = dataApi.language.filter((lang) => {
                 if (tutor.tu_id === lang.tu_id) {
                   return lang;
                 }
               });
-              console.log(tutor);
               return (
                 <Link
                   className={`bg-white relative w-full h-[250px] rounded-xl border-[1px] border-blackNot shadow-lg`}
                   href={`tutors/${tutor.tu_id}`}
+                  key={tutor.ad_title + tutor.tu_lastname}
                 >
                   <TutorsCards
                     key={tutor.ad_title + tutor.tu_lastname}
@@ -119,16 +133,63 @@ const GridTutors = ({ subject, level, min, max }) => {
             })}
           {!dataApi &&
             dummyData.map((data) => (
-              <div className="blur-lg bg-white relative w-full h-[250px] rounded-xl border-[1px] border-blackNot shadow-lg">
-                                <TutorsCards
-                  key={data.id}
-                  dummy={true}
-                  tutor={data}
-                ></TutorsCards>
-                </div>
+              <div
+                key={data.tu_id}
+                className="blur-lg bg-white relative w-full h-[250px] rounded-xl border-[1px] border-blackNot shadow-lg"
+              >
+                <TutorsCards dummy={true} tutor={data}></TutorsCards>
+              </div>
             ))}
           {!dataApi && <Loader show={dataApi ? false : true}></Loader>}
         </div>
+      </div>
+      <div className="px-[135px] py-[48px]">
+        <ul className="flex gap-x-12 justify-center items-center child:cursor-pointer">
+          <ChevronLeft
+            onClick={() => {
+              setSlice((prev) => {
+                if (prev.start > 0) {
+                  document.getElementById("tutors-container").scrollIntoView();
+                  return { start: prev.start - 6, end: prev.end - 6 };
+                } else {
+                  return { start: prev.start, end: prev.end };
+                }
+              });
+              if (defaultPage != 1) {
+                setDefaultPage((prev) => prev - 1);
+              }
+            }}
+            className="bg-main rounded-full text-white w-[45px] h-[45px]"
+          ></ChevronLeft>
+          {mapMe?.map((num) => (
+            <li
+              key={num}
+              className={`${
+                num + 1 === defaultPage
+                  ? "scale-[1.5] font-bold text-main"
+                  : "scale-[1] text-black"
+              } text-3xl transition-transform duration-300 transition-colors`}
+            >
+              {num + 1}
+            </li>
+          ))}
+          <ChevronRight
+            onClick={() => {
+              setSlice((prev) => {
+                if (prev.end < dataApi.response.length) {
+                  document.getElementById("tutors-container").scrollIntoView();
+                  return { start: prev.end, end: prev.end + 6 };
+                } else {
+                  return { start: prev.start, end: prev.end };
+                }
+              });
+              if (defaultPage != totalPages) {
+                setDefaultPage((prev) => prev + 1);
+              }
+            }}
+            className="bg-main rounded-full text-white w-[45px] h-[45px]"
+          ></ChevronRight>
+        </ul>
       </div>
     </section>
   );
