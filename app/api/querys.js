11f22cae3abc729f8ccfa9375a1879375c86e185
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-18 13:50:28 -0400

    modified queries to get levels of tutors

--- app/api/querys.js
+++ app/api/querys.js
@@ -1,232 +1,202 @@
 export const selectAdvertisements = (subject, min, max, level) => {
   return `
   SELECT
-  advertisements.advertisements_title AS ad_title,
   country.iso as country,
   tutors.tutor_description as tu_desc,
   tutors.tutor_skills as tu_skill,
-  advertisements.advertisements_description as ad_desc,
-  advertisements.advertisements_level as ad_level,
-  advertisements.advertisements_price as ad_price,
   tutors.tutor_name as tu_name,
   tutors.tutor_lastname as tu_lastname,
-  subjects.subject_name as subject,
+  jsonb_agg(subjects.subject_name) AS subject_names,
+  jsonb_agg(tutors_subjects.subject_level) AS subject_levels,
   round(AVG(reviews.review_rating)::numeric,2) AS average_rating,
   COUNT(reviews.review_rating) AS number_reviews,
   tutors.tutor_hourly_wage as tu_hourly,
   tutors.tutor_id as tu_id,
   tutors.tutor_first_free as first_class
 FROM
-  advertisements
+  tutors
 JOIN
-  tutors ON advertisements.tutor_id = tutors.tutor_id AND tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
-JOIN
-subjects ON advertisements.subject_id = subjects.subject_id AND subjects.subject_name = '${subject.toLowerCase()}' AND advertisements.advertisements_level = '${level.toLowerCase()}'
+  tutors_subjects ON tutors_subjects.tutor_id = tutors.tutor_id
 JOIN
   country ON tutors.country_id = country.id
 JOIN
   reviews ON reviews.tutor_id = tutors.tutor_id
+JOIN
+  subjects ON subjects.subject_id = tutors_subjects.subject_id AND subjects.subject_name = '${subject.toLowerCase()}'
+  WHERE tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
 GROUP BY
-  advertisements.advertisements_title,
   country.iso,
   tutors.tutor_description,
   tutors.tutor_skills,
-  advertisements.advertisements_description,
-  advertisements.advertisements_level,
-  advertisements.advertisements_price,
   tutors.tutor_name,
   tutors.tutor_lastname,
-  subjects.subject_name,
   tutors.tutor_hourly_wage,
   tutors.tutor_id,
-  tutors.tutor_first_free
+  tutors.tutor_first_free,
+  subjects.subject_name
+  HAVING
+  '${level.toLowerCase()}' = ANY(ARRAY_AGG(tutors_subjects.subject_level))
 ORDER BY
-  AVG(reviews.review_rating) DESC;
+AVG(reviews.review_rating) DESC;
   `;
 };
 
 export const selectAdvertisementsAll = (min, max, level) => {
   return `
   SELECT
-  advertisements.advertisements_title AS ad_title,
   country.iso as country,
   tutors.tutor_description as tu_desc,
   tutors.tutor_skills as tu_skill,
-  advertisements.advertisements_description as ad_desc,
-  advertisements.advertisements_level as ad_level,
-  advertisements.advertisements_price as ad_price,
   tutors.tutor_name as tu_name,
   tutors.tutor_lastname as tu_lastname,
-  subjects.subject_name as subject,
+  jsonb_agg(subjects.subject_name) AS subject_names,
+  jsonb_agg(tutors_subjects.subject_level) AS subject_levels,
   round(AVG(reviews.review_rating)::numeric,2) AS average_rating,
   COUNT(reviews.review_rating) AS number_reviews,
   tutors.tutor_hourly_wage as tu_hourly,
   tutors.tutor_id as tu_id,
   tutors.tutor_first_free as first_class
 FROM
-  advertisements
+  tutors
 JOIN
-  tutors ON advertisements.tutor_id = tutors.tutor_id AND tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
-JOIN
-subjects ON advertisements.subject_id = subjects.subject_id AND advertisements.advertisements_level = '${level.toLowerCase()}'
+  tutors_subjects ON tutors_subjects.tutor_id = tutors.tutor_id
 JOIN
   country ON tutors.country_id = country.id
 JOIN
   reviews ON reviews.tutor_id = tutors.tutor_id
+JOIN
+  subjects ON subjects.subject_id = tutors_subjects.subject_id
 GROUP BY
-  advertisements.advertisements_title,
   country.iso,
   tutors.tutor_description,
   tutors.tutor_skills,
-  advertisements.advertisements_description,
-  advertisements.advertisements_level,
-  advertisements.advertisements_price,
   tutors.tutor_name,
   tutors.tutor_lastname,
-  subjects.subject_name,
   tutors.tutor_hourly_wage,
   tutors.tutor_id,
-  tutors.tutor_first_free
+  tutors.tutor_first_free,
+  subjects.subject_name
 ORDER BY
-  AVG(reviews.review_rating) DESC;
+AVG(reviews.review_rating) DESC;
   `;
 };
 export const selectAdvertisementsLevel = (subject, min, max) => {
   return `
   SELECT
-  advertisements.advertisements_title AS ad_title,
   country.iso as country,
   tutors.tutor_description as tu_desc,
   tutors.tutor_skills as tu_skill,
-  advertisements.advertisements_description as ad_desc,
-  advertisements.advertisements_level as ad_level,
-  advertisements.advertisements_price as ad_price,
   tutors.tutor_name as tu_name,
   tutors.tutor_lastname as tu_lastname,
-  subjects.subject_name as subject,
+  jsonb_agg(subjects.subject_name) AS subject_names,
+  jsonb_agg(tutors_subjects.subject_level) AS subject_levels,
   round(AVG(reviews.review_rating)::numeric,2) AS average_rating,
   COUNT(reviews.review_rating) AS number_reviews,
   tutors.tutor_hourly_wage as tu_hourly,
   tutors.tutor_id as tu_id,
   tutors.tutor_first_free as first_class
 FROM
-  advertisements
+  tutors
 JOIN
-  tutors ON advertisements.tutor_id = tutors.tutor_id AND tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
-JOIN
-subjects ON advertisements.subject_id = subjects.subject_id AND subjects.subject_name = '${subject.toLowerCase()}'
+  tutors_subjects ON tutors_subjects.tutor_id = tutors.tutor_id
 JOIN
   country ON tutors.country_id = country.id
 JOIN
   reviews ON reviews.tutor_id = tutors.tutor_id
+JOIN
+  subjects ON subjects.subject_id = tutors_subjects.subject_id AND subjects.subject_name = '${subject.toLowerCase()}'
+  WHERE tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
 GROUP BY
-  advertisements.advertisements_title,
   country.iso,
   tutors.tutor_description,
   tutors.tutor_skills,
-  advertisements.advertisements_description,
-  advertisements.advertisements_level,
-  advertisements.advertisements_price,
   tutors.tutor_name,
   tutors.tutor_lastname,
-  subjects.subject_name,
   tutors.tutor_hourly_wage,
   tutors.tutor_id,
-  tutors.tutor_first_free
+  tutors.tutor_first_free,
+  subjects.subject_name
+
 ORDER BY
-  AVG(reviews.review_rating) DESC;
+AVG(reviews.review_rating) DESC;
   `;
 };
 export const selectAdvertisementsMin = (min, max) => {
   return `
   SELECT
-  advertisements.advertisements_title AS ad_title,
   country.iso as country,
   tutors.tutor_description as tu_desc,
   tutors.tutor_skills as tu_skill,
-  advertisements.advertisements_description as ad_desc,
-  advertisements.advertisements_level as ad_level,
-  advertisements.advertisements_price as ad_price,
   tutors.tutor_name as tu_name,
   tutors.tutor_lastname as tu_lastname,
-  subjects.subject_name as subject,
+  jsonb_agg(subjects.subject_name) AS subject_names,
+  jsonb_agg(tutors_subjects.subject_level) AS subject_levels,
   round(AVG(reviews.review_rating)::numeric,2) AS average_rating,
   COUNT(reviews.review_rating) AS number_reviews,
   tutors.tutor_hourly_wage as tu_hourly,
   tutors.tutor_id as tu_id,
   tutors.tutor_first_free as first_class
 FROM
-  advertisements
+  tutors
 JOIN
-  tutors ON advertisements.tutor_id = tutors.tutor_id AND tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
-JOIN
-subjects ON advertisements.subject_id = subjects.subject_id
+  tutors_subjects ON tutors_subjects.tutor_id = tutors.tutor_id
 JOIN
   country ON tutors.country_id = country.id
 JOIN
   reviews ON reviews.tutor_id = tutors.tutor_id
+JOIN
+  subjects ON subjects.subject_id = tutors_subjects.subject_id
+  WHERE tutors.tutor_hourly_wage BETWEEN ${min}::money AND ${max}::money
 GROUP BY
-  advertisements.advertisements_title,
   country.iso,
   tutors.tutor_description,
   tutors.tutor_skills,
-  advertisements.advertisements_description,
-  advertisements.advertisements_level,
-  advertisements.advertisements_price,
   tutors.tutor_name,
   tutors.tutor_lastname,
-  subjects.subject_name,
   tutors.tutor_hourly_wage,
   tutors.tutor_id,
-  tutors.tutor_first_free
+  tutors.tutor_first_free,
+
 ORDER BY
-  AVG(reviews.review_rating) DESC;
+AVG(reviews.review_rating) DESC;
   `;
 };
 export const selectAnyAdvertisements = () => {
   return `
   SELECT
-  advertisements.advertisements_title AS ad_title,
-  country.iso AS country,
-  tutors.tutor_description AS tu_desc,
-  tutors.tutor_skills AS tu_skill,
-  advertisements.advertisements_description as ad_desc,
-  advertisements.advertisements_level as ad_level,
-  advertisements.advertisements_price as ad_price,
+  country.iso as country,
+  tutors.tutor_description as tu_desc,
+  tutors.tutor_skills as tu_skill,
   tutors.tutor_name as tu_name,
   tutors.tutor_lastname as tu_lastname,
-  subjects.subject_name as subject,
-  round(AVG(reviews.review_rating)::numeric, 2) AS average_rating,
+  jsonb_agg(subjects.subject_name) AS subject_names,
+  jsonb_agg(tutors_subjects.subject_level) AS subject_levels,
+  round(AVG(reviews.review_rating)::numeric,2) AS average_rating,
   COUNT(reviews.review_rating) AS number_reviews,
   tutors.tutor_hourly_wage as tu_hourly,
   tutors.tutor_id as tu_id,
   tutors.tutor_first_free as first_class
 FROM
-  advertisements
+  tutors
 JOIN
-  tutors ON advertisements.tutor_id = tutors.tutor_id
-JOIN
-  subjects ON advertisements.subject_id = subjects.subject_id
+  tutors_subjects ON tutors_subjects.tutor_id = tutors.tutor_id
 JOIN
   country ON tutors.country_id = country.id
 JOIN
   reviews ON reviews.tutor_id = tutors.tutor_id
+JOIN
+  subjects ON subjects.subject_id = tutors_subjects.subject_id
 GROUP BY
-  advertisements.advertisements_title,
   country.iso,
   tutors.tutor_description,
   tutors.tutor_skills,
-  advertisements.advertisements_description,
-  advertisements.advertisements_level,
-  advertisements.advertisements_price,
   tutors.tutor_name,
   tutors.tutor_lastname,
-  subjects.subject_name,
   tutors.tutor_hourly_wage,
   tutors.tutor_id,
   tutors.tutor_first_free
 ORDER BY
-  average_rating DESC;
+AVG(reviews.review_rating) DESC;
   `;
 };
 export const selectTutorLanguages = () => {
