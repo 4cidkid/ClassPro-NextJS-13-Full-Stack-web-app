f16eb570c95b3ade852b94f834c3171ffa200495
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-20 22:32:08 -0400

    start re-making the s3 implementation

--- app/api/s3url/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import generateUploadUrl from "../s3"; 
-export async function GET() {
-    const url = await generateUploadUrl()
-    return NextResponse.json({url})
-}
