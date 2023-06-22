68b7258bc1e073cbe7a26e7da61057dbbb309cba
Nicolas Torres <desmondgamesyt@gmail.com>
2023-06-22 15:30:31 -0400

    updated fetch urls

--- app/tutors/search.js
+++ app/tutors/search.js
@@ -2,7 +2,7 @@
 //function to search for tutors with given params
 export async function searchFor({ subject, level, min, max }) {
   const response = await fetch(
-    `http://localhost:3000/api/tutors?subject=${subject}&level=${level}&min=${min}&max=${max}`
+    `/api/tutors?subject=${subject}&level=${level}&min=${min}&max=${max}`
   );
   const data = await response.json();
   if(!data.response.length){
@@ -13,13 +13,13 @@ export async function searchFor({ subject, level, min, max }) {
 }
 //search any tutor, without any params
 export async function searchAny() {
-  const response = await fetch(`http://localhost:3000/api/tutors`);
+  const response = await fetch(`/api/tutors`);
   const data = await response.json();
   return data;
 }
 
 export async function getLanguages(){
-  const response = await fetch('http://localhost:3000/api/languages')
+  const response = await fetch('/api/languages')
   const data = await response.json()
   return data;
 }
