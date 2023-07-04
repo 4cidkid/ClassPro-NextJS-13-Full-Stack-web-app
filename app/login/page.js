61182eced04b4234c40708029d8bebcb86631882
Nicolas Torres <desmondgamesyt@gmail.com>
2023-07-03 21:26:55 -0400

    added login

---
+++ app/login/page.js
@@ -0,0 +1,9 @@
+export default async function Login(){
+    return(
+        <section>
+            <form action="/api/auth" method="POST">
+                <button type="submit"></button>
+                </form>
+        </section>
+    )
+}
