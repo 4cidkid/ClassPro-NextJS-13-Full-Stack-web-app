a2d405f32774edb1e8bf8fb2941d6b632207828f
Nicolas Torres <desmondgamesyt@gmail.com>
2023-07-06 20:42:36 -0400

    added route to insert users

---
+++ app/api/users/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import pool from "../db";
+import dotenv from "dotenv";
+dotenv.config({ path: "app/api/.env" });
+import CryptoJS from "crypto-js";
+export async function POST(req, res) {
+  
+}
