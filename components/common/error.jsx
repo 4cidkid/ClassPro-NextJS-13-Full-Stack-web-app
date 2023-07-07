61250e181918884d5c87f4fe9d2506492e91e2d4
Nicolas Torres <desmondgamesyt@gmail.com>
2023-07-07 18:16:38 -0400

    modified error component to receive classname props

--- components/common/error.jsx
+++ components/common/error.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { AlertTriangle } from 'react-feather'
 
-export const Error = ({error}) => {
+export const Error = ({error,classname}) => {
     return(
-        <div className='capitalize flex items-center text-2xl font-bold text-white justify-center gap-5 appearError fixed right-[20px] bottom-[20px] bg-main w-[450px] h-[90px] shadow-xl border border-[rgba(0,0,0,0.2)]'>
+        <div className={`${classname || 'text-white'} capitalize flex items-center text-2xl font-bold  justify-center gap-5 appearError fixed right-[20px] bottom-[20px] bg-main w-[450px] h-[90px] shadow-xl border border-[rgba(0,0,0,0.2)]`}>
             <AlertTriangle>
             </AlertTriangle>
             {error || 'unexpected error occurred'}
