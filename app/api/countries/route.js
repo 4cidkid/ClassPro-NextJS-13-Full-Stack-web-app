b658f89b729266edf78c57f7e90b1321d3f1b969
Nicolas Torres <desmondgamesyt@gmail.com>
2023-07-06 20:40:54 -0400

    added custom msg and status if db returns an error

--- app/api/countries/route.js
+++ app/api/countries/route.js
@@ -4,5 +4,10 @@ export async function GET() {
   //get all subjects
   try{
     const data = await pool.query('SELECT * FROM country')
-  return NextResponse.json({ countries: data.rows });
+    return NextResponse.json({ countries: data.rows },{status:200});
+  }catch(e){
+    if(e){
+      return NextResponse.json({ msg: "Couldn't get countries " + e.message},{status:500});
+    }
+  }
 }
